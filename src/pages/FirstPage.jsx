51e0cfa12f89a3b989bc28ee043e498756bd726c
Alejandro <legorios@gmail.com>
2022-11-01 16:59:00 -0500

    documentacion en componente FirstPage

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -4,10 +4,17 @@ import React, { useState } from "react"
 export const FirstPage = () => {
   const [valor, setValor] = useState(10) //Sirve para disparar estados
 
+  /**
+   * "modificarEstado" is a function that takes a number as an argument and returns the maximum of the
+   * sum of the current value of "valor" and the number passed as an argument and 0.
+   */
   const modificarEstado = (num) => {
     setValor(Math.max(valor + num, 0))
   }
 
+  /**
+   * The reset function sets the value of the valor variable to 10.
+   */
   const reset = () => {
     setValor(10)
   }
@@ -23,3 +30,4 @@ export const FirstPage = () => {
     </>
   )
 } 
+// por favor 
