90607134a13541b60f4855d181a267ef7c861884
Alejandro <legorios@gmail.com>
2022-11-01 17:06:13 -0500

    create useContador

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -1,24 +1,9 @@
-import React, { useState } from "react"
+import { useContador } from "../hooks/useContador"
 
 //useState
+// REFACTOR
 export const FirstPage = () => {
-  const [valor, setValor] = useState(10) //Sirve para disparar estados
-
-  /**
-   * "modificarEstado" is a function that takes a number as an argument and returns the maximum of the
-   * sum of the current value of "valor" and the number passed as an argument and 0.
-   */
-  const modificarEstado = (num) => {
-    setValor(Math.max(valor + num, 0))
-  }
-
-  /**
-   * The reset function sets the value of the valor variable to 10.
-   */
-  const reset = () => {
-    setValor(10)
-  }
-
+  const { valor, modificarEstado, reset} = useContador(200)
   return (
     <>
       <h1>Contador mejorado</h1>
